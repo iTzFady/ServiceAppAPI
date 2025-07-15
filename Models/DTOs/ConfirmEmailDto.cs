77748f73880ede404e33bd0fd7fe476fee3f7844
Faaady <47241432+iTzFady@users.noreply.github.com>
2025-07-15 13:51:53 +0300

    Update ConfirmEmailDto.cs

--- Models/DTOs/ConfirmEmailDto.cs
+++ Models/DTOs/ConfirmEmailDto.cs
@@ -3,8 +3,9 @@ using System.ComponentModel.DataAnnotations;
 public class ConfirmEmailDto
 {
     [Required]
+    public string Token { get; set; }
+    [Required]
     [EmailAddress]
     public string Email { get; set; }
-    [Required]
-    public string Token { get; set; }
+
 }
