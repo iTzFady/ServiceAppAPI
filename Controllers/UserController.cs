5166c37e9784f2f1c6890ce8c81c211b6d10fa19
Faaady <47241432+iTzFady@users.noreply.github.com>
2025-07-15 14:03:37 +0300

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -130,7 +130,7 @@ namespace ServiceApp.Controllers
                 }
             });
         }
-        [HttpPost("confirm-email")]
+        [HttpGet("confirm-email")]
         public async Task<IActionResult> ConfirmEmail([FromQuery] ConfirmEmailDto dto)
         {
             var user = await _db.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
