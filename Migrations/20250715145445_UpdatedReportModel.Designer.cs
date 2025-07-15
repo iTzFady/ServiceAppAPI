6dcd5806d46c059cf43e7d06d277f321ff9aa151
Faaady <47241432+iTzFady@users.noreply.github.com>
2025-07-15 17:55:08 +0300

    Report Migration

--- Migrations/20250715145445_UpdatedReportModel.Designer.cs
+++ Migrations/20250715145445_UpdatedReportModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ServiceApp.Data;
@@ -11,9 +12,11 @@ using ServiceApp.Data;
 namespace ServiceApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250715145445_UpdatedReportModel")]
+    partial class UpdatedReportModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250715145445_UpdatedReportModel.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedReportModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_ReportedByUserId",
+                table: "Reports",
+                column: "ReportedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_ReportedUserId",
+                table: "Reports",
+                column: "ReportedUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Users_ReportedByUserId",
+                table: "Reports",
+                column: "ReportedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Users_ReportedUserId",
+                table: "Reports",
+                column: "ReportedUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Users_ReportedByUserId",
+                table: "Reports");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Users_ReportedUserId",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_ReportedByUserId",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_ReportedUserId",
+                table: "Reports");
+        }
+    }
+}
